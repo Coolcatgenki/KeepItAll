51f49a46f76f42b5768087f1bbfa4f7f1e8385c6
Sugashima Alegria Felicidad <steinsgatedo@hotmail.com>
2023-06-06 23:21:52 +0900

    mega commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ root.render(
           authType={"cookie"}
           authName={"_auth"}
           cookieDomain={window.location.hostname}
-          cookieSecure={false}
+          cookieSecure={true}
         >  
     <BrowserRouter>
       <Router/>
